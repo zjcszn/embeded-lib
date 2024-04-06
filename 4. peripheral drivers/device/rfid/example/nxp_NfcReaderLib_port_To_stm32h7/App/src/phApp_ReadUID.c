359238d179472daf1ec72954a295ddf0f879f71c
zjcszn <zjcszn@gmail.com>
2024-04-06 23:48:18 +0800

    update id card uid example for clrc663

--- 4. peripheral drivers/device/rfid/example/nxp_NfcReaderLib_port_To_stm32h7/App/src/phApp_ReadUID.c
+++ 4. peripheral drivers/device/rfid/example/nxp_NfcReaderLib_port_To_stm32h7/App/src/phApp_ReadUID.c
@@ -1,40 +1,40 @@
 phStatus_t phApp_ReadUID(void) {
     phStatus_t status = PH_ERR_SUCCESS;
-    uint8_t bAtqbLen;
-    uint8_t aAtqb[13];
-    uint8_t aCmd[9] = {0x1d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x01, 0x08};
+    uint8_t aAttirCmd[9] = {0x1d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x01, 0x08};
+    uint8_t aQueryCmd[5] = {0x00, 0x36, 0x00, 0x00, 0x08};
     uint8_t *pResp = NULL;
-    uint16_t wRespLength;
+    uint16_t wRespLength = 0;
+    uint8_t bAtqbLen = 0;
+    uint8_t aAtqb[13];
     uint8_t bErrCnt = 0;
     phpalI14443p3b_Sw_DataParams_t *pPal14443B = phNfcLib_GetDataParams(PH_COMP_PAL_ISO14443P3B);
     
     do {
+        
         if (bErrCnt > 10) {
-            DEBUG_PRINTF("[ERROR]: read uid of id card failed");
+//            DEBUG_PRINTF("[ERROR]: read uid of id card failed\r\n");
             break;
         }
         
-        /*
+        phhalHw_FieldReset(pHal);
+        
         status = phpalI14443p3b_RequestB(pPal14443B, 0, 0, 0, aAtqb, &bAtqbLen);
         if ((status != PH_ERR_SUCCESS) || ((bAtqbLen != 12) && (bAtqbLen != 13))) {
+//            DEBUG_PRINTF("[%u] requestB error, stats: %04X, atqlen: %uc\r\n", bErrCnt, status, bAtqbLen);
             bErrCnt++;
             continue;
         }
-        */
         
-        status = phhalHw_Exchange(pPal14443B->pHalDataParams, PH_EXCHANGE_BUFFER_LAST, aCmd, 9, &pResp, &wRespLength);
+        status = phhalHw_Exchange(pPal14443B->pHalDataParams, PH_EXCHANGE_BUFFER_LAST, aAttirCmd, 9, &pResp, &wRespLength);
         if ((status != PH_ERR_SUCCESS) || (wRespLength != 1) || (pResp[0] != 0x08)) {
+//            DEBUG_PRINTF("[%u] attriB error, stats: %04X, resplen: %u\r\n", bErrCnt, status, wRespLength);
             bErrCnt++;
             continue;
         }
         
-        aCmd[0] = 0x00;
-        aCmd[1] = 0x36;
-        aCmd[2] = 0x00;
-        aCmd[3] = 0x00;
-        aCmd[4] = 0x08;
-        status = phhalHw_Exchange(pPal14443B->pHalDataParams, PH_EXCHANGE_BUFFER_LAST, aCmd, 5, &pResp, &wRespLength);
+        status = phhalHw_Exchange(pPal14443B->pHalDataParams, PH_EXCHANGE_BUFFER_LAST, aQueryCmd, 5, &pResp, &wRespLength);
         if ((status != PH_ERR_SUCCESS) || (wRespLength != 10) || (pResp[8] != 0x90) || (pResp[9] != 0x00)) {
+//            DEBUG_PRINTF("[%u] query uid error, stats: %04X, resplen: %u\r\n", bErrCnt, status, wRespLength);
             bErrCnt++;
             continue;
         } else {
