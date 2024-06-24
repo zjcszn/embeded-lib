13f4e086ea863f6171e9a06ddc382075997a6540
zjcszn <zjcszn@gmail.com>
2024-06-24 22:52:23 +0800

    update multi task

--- 1. library/1.3 timer/1.3.2 multi task/multi_task.c
+++ 1. library/1.3 timer/1.3.2 multi task/multi_task.c
@@ -1,168 +1,123 @@
 #include "multi_task.h"
 #include <string.h>
 
-/************************ 宏定义 ***********************/
 
-#define __IO volatile
+#define __IO          volatile
 
-#define TASK_MAX_NUM    (10U)   // 最大任务数量
+typedef struct task_handle task_handle_t;
 
-/********************* 静态全局变量 *********************/
+struct task_handle{
+  entry_t entry;       
+  void* args;        
+  uint16_t  delay;       
+  uint16_t  period;      
+  uint32_t  deadline;    
+  task_handle_t *next;         
+};
 
-// 任务堆空间
-static task_t  task_heap[TASK_MAX_NUM] = {0};
+static task_handle_t  tcb[TASK_MAX_NUM];
+static task_handle_t *task_list;
+static __IO uint32_t  task_ticks;
 
-// 任务调度队列
-static task_t* task_list = NULL;
 
-// 任务Ticks
-static __IO uint64_t task_ticks = 0;
+static void task_insert(task_handle_t *task) {
+  task_handle_t  **p;
+  uint32_t  now = task_ticks;
 
-/********************* 私有函数声明 *********************/
+  for (p = &task_list; ; p = &(*p)->next) {
+    if (!(*p)) {
+        task->next = NULL;
+        *p = task;
+        break;
+    }
+    if ((int)(task->deadline - now) < (int)((*p)->deadline - now)) {
+        task->next = *p;
+        *p = task;
+        break;
+    }
+  }
+}
 
-static void    task_insert(task_t *new_task);
-static task_t* task_malloc(void);
-static void    task_free(task_t *task);
-
-/*********************** 函数定义 **********************/
-
-/**
- * @brief Ticks更新函数，定时器内运行
- * 
- */
 void task_ticks_update(void) {
   task_ticks++;
 }
 
-
 /**
- * @brief 添加一个新任务,并插入任务调度队列
+ * @brief 删除任务
  * 
- * @param entry 任务入口函数
- * @param args 传入参数
- * @param delay 延时时间
- * @param period 循环周期
- * @return int 添加失败返回-1，添加成功返回0
+ * @param entry 任务入口
+ * @return int 0表示删除失败，1表示删除失败（未找到该任务）
  */
-int task_add(void(*entry)(void *args), void *args, uint16_t delay, uint16_t period) {
-  task_t *new_task = task_malloc();
-  if (new_task == NULL) return -1;
-  new_task->entry   = entry;
-  new_task->args    = args;
-  new_task->delay   = delay;
-  new_task->period  = period;
-  new_task->timeout = task_ticks + delay;
-  task_insert(new_task);
-  return 0;
-}
-
-/**
- * @brief 将一个任务按序插入任务调度队列中
- * 
- * @param task 
- */
-static void task_insert(task_t *task) {
-  task_t **target;
-  for (target = &task_list;; target = &(*target)->next) {
-    if (!*target) {
-        task->next = NULL;
-        *target = task;
-        break;
-    }
-    if (task->timeout < (*target)->timeout) {
-        task->next = *target;
-        *target = task;
-        break;
-    }
-  }
-}
-
-/**
- * @brief 从任务调度队列中删除一个任务
- * 
- * @param entry 待删除的任务函数指针
- * @return int 删除失败返回-1，删除成功返回0
- */
-int task_del(void(*entry)(void *args)) {
-  task_t** target = &task_list;
-  for (; *target; target = &(*target)->next) {
-    if ((*target)->entry == entry) {
-      task_t *del_task = *target;
-      *target = (*target)->next;
-      task_free(del_task);
+int task_delete(entry_t entry) {
+  task_handle_t** p;
+  task_handle_t*  del;
+   
+  for (p = &task_list; *p; p = &((*p)->next)) {
+    if ((*p)->entry == entry) {
+      del = *p;
+      *p = (*p)->next;
+      memset(del, 0x00, sizeof(task_handle_t));
       return 0;
     }
   }
-  return -1;
+
+  return 1;
 }
 
 /**
- * @brief 在任务调度队列中查找指定任务
+ * @brief 创建任务
  * 
- * @param entry 
- * @return task_t* 任务空间首地址
+ * @param entry 任务入口
+ * @param args 传入参数
+ * @param delay 延迟时间 ms
+ * @param period 调度周期
+ * @return int 0表示创建成功， 1表示创建失败
  */
-task_t* task_find(void(*entry)(void *args)) {
-  task_t** target = &task_list;
-  for (; *target; target = &(*target)->next) {
-    if ((*target)->entry == entry) {
-      return *target;
+int task_create(entry_t entry, void *args, uint16_t delay, uint16_t period) {
+  task_handle_t *new = NULL;
+
+  task_delete(entry); /* 如果任务已在列表中，则删除旧任务 */
+
+  for (int i = 0; i < TASK_MAX_NUM; i++) {
+    if(tcb[i].entry == NULL) {
+      new = &tcb[i];
+      break;
     }
   }
-  return NULL;
+
+  if (new != NULL) {
+    new->entry    = entry;
+    new->args     = args;
+    new->delay    = delay;
+    new->period   = period;
+    new->deadline = task_ticks + delay;
+
+    task_insert(new);
+    return 0;
+  }
+
+  return 1;
 }
 
-/**
- * @brief 更新任务循环周期
- * 
- * @param task 
- * @param period 
- */
-void task_period_update(task_t *task, uint16_t period) {
-  task->period = period;
-}
 
-/**
- * @brief 任务调度处理程序
- * 
- */
-void task_poll(void) {
-  task_t *target = task_list;
-  for (; target; target = target->next) {
-    if(task_ticks < target->timeout) {
+
+
+void task_scheduler(void) {
+  task_handle_t  *p;
+  uint32_t now = task_ticks;
+
+  for (p = task_list; p; p = p->next) {
+    if((int)(p->deadline - now) > 0) {
       return;
     }
-    task_list = target->next;
-    if (target->period) {
-      target->timeout = task_ticks + target->period;
-      task_insert(target);
+
+    task_list = p->next;
+    if (p->period) {
+      p->deadline = task_ticks + p->period;
+      task_insert(p);
     }
-    target->entry(target->args);
+
+    p->entry(p->args);
   }
 }
 
-
-/**
- * @brief 任务堆分配函数，从定义的堆数组内寻找可用空间，并分配给新任务
- * 
- * @return task_t* 任务空间首地址
- */
-static task_t* task_malloc(void) {
-  for (int i = 0; i < TASK_MAX_NUM; i++) {
-    if(task_heap[i].entry == NULL) {
-      return &task_heap[i];
-    }
-  }
-  return NULL;
-}
-
-/**
- * @brief 任务堆释放函数，释放不使用的任务空间
- * 
- * @param task 
- */
-static void task_free(task_t *task) {
-  if (task != NULL) {
-    memset(task, 0x00, sizeof(task_t));
-  }
-}
--- 1. library/1.3 timer/1.3.2 multi task/multi_task.h
+++ 1. library/1.3 timer/1.3.2 multi task/multi_task.h
@@ -7,30 +7,19 @@
 extern "C" {  
 #endif
 
-/********************* 类型声明 *********************/
 
-// 任务入口函数指针
+#define TASK_MAX_NUM    (10U)   // 最大任务数量
+
+
 typedef void (*entry_t)(void *args);
-// 任务结构体类型
-typedef struct _task task_t;
 
-struct _task {
-  entry_t   entry;      // 任务入口函数
-  void*     args;       // 任务参数
-  uint16_t  delay;      // 延迟时间
-  uint16_t  period;     // 间隔周期
-  task_t*   next;       // next指针    
-  uint64_t  timeout;    // 超时时间  
-};
 
-/********************* 函数声明 *********************/
+
 
 void task_ticks_update(void);
-int  task_add(void(*entry)(void *args), void *args, uint16_t delay, uint16_t period);
-int  task_del(void(*entry)(void *args));
-task_t* task_find(void(*entry)(void *args));
-void task_period_update(task_t *task, uint16_t period);
-void task_poll(void);
+int  task_create(entry_t entry, void *args, uint16_t delay, uint16_t period);
+int  task_delete(entry_t entry);
+void task_scheduler(void);
 
 #ifdef __cplusplus  
 }  
